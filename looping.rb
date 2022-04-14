102b1ecaa454019f1dd3a8ce200422b2a66b1ad1
gerry <hkggerry@gmail.com>
2022-04-14 01:15:37 -0700

    commit tree ef990ff2423b25022668531cd26a6c618773d162

--- looping.rb
+++ looping.rb
@@ -1,5 +1,10 @@
 def happy_new_year
-  # your code here
+  i = -11
+  while i < -1
+    i += 1
+    puts "#{i}"
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
